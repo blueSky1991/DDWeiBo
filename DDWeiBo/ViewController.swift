b994efc15fb49f37feed9c4269295034c214cf1c
blueSky1991 <m18210681011@163.com>
2016-05-06 08:47:01 +0800

    张东东
    建立初始化的工程

---
+++ DDWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDWeiBo
+//
+//  Created by 张东东 on 16/5/6.
+//  Copyright © 2016年 张东东. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
